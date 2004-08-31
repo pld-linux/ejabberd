8a2a99a4d9e78b4e9028c085e09a340b60806ebd
Jacek Konieczny <jajcus@pld-linux.org>
2004-08-31 20:56:37 +0000

    - ejabberctl tool -  PLD version of a script starting it
    Changed files:
        ejabberdctl.sh -> 1.1

---
+++ ejabberdctl.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd /var/lib/ejabberd || exit $?
+
+COMMAND="erl -pa /usr/lib/ejabberd/ebin \
+      -noinput \
+      -sname ejabberdctl \
+      -s ejabberd_ctl \
+      -extra $@"
+
+if [ "`id -u`" -eq "0" ] ; then
+	exec su -s /bin/sh jabber -c "exec $COMMAND"
+else
+	eval "exec $COMMAND"
+fi
